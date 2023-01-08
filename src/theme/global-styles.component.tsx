9e813d896268d8bfeb85fe53cd655296496fc9be
Filip Balada <flpbalada@gmail.com>
2023-01-08 16:57:16 +0100

    add default font-family to global-styles

--- src/theme/global-styles.component.tsx
+++ src/theme/global-styles.component.tsx
@@ -1,6 +1,8 @@
 import { createGlobalStyle, css } from 'styled-components'
 
 const bodyStyles = css`
+	font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+		'Helvetica Neue', sans-serif;
 	margin: 0;
 	padding: 0;
 `
